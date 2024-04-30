ec77b77b08fb1cc98ca28e7695f4445da5d7d711
kenux196 <kenux.yun@gmail.com>
2024-05-01 00:56:39 +0900

    test language

--- src/layouts/FooterLayout.vue
+++ src/layouts/FooterLayout.vue
@@ -5,12 +5,55 @@
       Released under <span class="text-weight-bold">MIT License</span>.
       <div>Copyright @ 2024 kenux.yun</div>
     </div>
-
-    <div class="col-5 text-right q-mr-sm">Quasar v{{ $q.version }}</div>
+    <div class="row col">
+      <div class="col-1">
+        <q-icon name="language"></q-icon>
+      </div>
+      <div class="col">
+        <q-select
+          v-model="lang"
+          :options="langOptions"
+          dense
+          borderless
+          emit-value
+          map-options
+          options-dense
+          style="margin-top: -9px"
+        />
+      </div>
+    </div>
+    <div class="col-1 q-mr-sm">Quasar v{{ $q.version }}</div>
   </q-footer>
 </template>
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import { useQuasar } from 'quasar';
+import languages from 'quasar/lang/index.json';
 
 const now = ref(new Date().toLocaleString());
+const appLanguages = languages.filter((lang) => ['ko-KR', 'en-US'].includes(lang.isoName));
+
+const langOptions = appLanguages.map((lang) => ({
+  label: lang.nativeName,
+  value: lang.isoName,
+}));
+
+const $q = useQuasar();
+const lang = ref($q.lang.isoName);
+
+watch(lang, (val) => {
+  // dynamic import, so loading on demand only
+  import(
+    /* webpackInclude: /(de|en-US)\.js$/ */
+    'quasar/lang/' + val
+  ).then((lang) => {
+    $q.lang.set(lang.default);
+  });
+});
 </script>
+<style>
+.lang-selector {
+  width: 100px;
+  margin: auto;
+}
+</style>
