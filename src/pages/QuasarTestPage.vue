8e8cf366e19241e0020fc1a0beedc86eb66f2a86
kenux196 <kenux.yun@gmail.com>
2024-05-01 21:31:40 +0900

    style

--- src/pages/QuasarTestPage.vue
+++ src/pages/QuasarTestPage.vue
@@ -19,6 +19,45 @@
     <div class="text-bold">{{ currentLocale }}</div>
     <div class="text-bold">{{ $t('jiwon') }}</div>
   </div>
+  <div style="width: 60%; border: 1px; border-style: dashed">
+    <div class="text-h1 text-primary">Headlin1</div>
+    <div class="text-h2 text-secondary">Headlin2</div>
+    <div class="text-h3 text-accent">Headlin3</div>
+    <div class="text-h4 text-dark">Headlin4</div>
+    <div class="text-h5 text-positive">Headlin5</div>
+    <div class="text-h6 text-negative">Headlin6</div>
+    <div class="text-subtitle1 text-info">Subtitle1</div>
+    <div class="text-subtitle2 text-warning">Subtitle2</div>
+    <div class="text-body1 bg-negative text-white">Body1. this is text-body1.</div>
+    <div class="text-body2 bg-secondary text-white">Body2. this is text-body2.</div>
+    <div class="text-right">text-right</div>
+    <div class="text-left">text-left</div>
+    <div class="text-center">text-center</div>
+    <div class="text-justify">text-justify</div>
+    <div class="bg-positive">
+      <span class="text-bold">text-bold / </span>
+      <span class="text-italic">text-italic / </span>
+      <span class="text-strike">text-strike / </span>
+      <span class="text-uppercase">text-upppercase / </span>
+      <span class="text-lowercase">Text-Lowercase / </span>
+      <span class="text-capitalize">text-capitalize / </span>
+    </div>
+  </div>
+  <div>
+    <div class="text-h5">Spacing</div>
+    <div class="q-mx-auto">margin x auto</div>
+    <div class="q-mx-xs">margin x xs</div>
+    <div class="q-mx-sm">margin x sm</div>
+    <div class="q-mx-md">margin x md</div>
+    <div class="q-mx-lg">margin x lg</div>
+    <div class="q-mx-xl">margin x xl</div>
+  </div>
+  <div style="width: 200px; height: 100px; background-color: brown">
+    <div class="fit bg-primary text-white text-center">Size : fit -> 영역 전체를 가득 채운다.</div>
+  </div>
+  <div style="width: 200px; height: 100px; background-color: brown">
+    <div class="full-height bg-secondary text-white text-center">Size : full-heigt -> 높이를 가득 체운다.</div>
+  </div>
 </template>
 
 <script setup>
