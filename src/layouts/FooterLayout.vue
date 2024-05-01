a0b89e08af1f7b5e3c530545771dfc94df95418f
kenux196 <kenux.yun@gmail.com>
2024-05-01 20:20:47 +0900

    modify language selector

--- src/layouts/FooterLayout.vue
+++ src/layouts/FooterLayout.vue
@@ -6,48 +6,14 @@
       <div>Copyright @ 2024 kenux.yun</div>
     </div>
     <div class="col">
-      <q-select
-        v-model="locale"
-        :options="localeOptions"
-        dark
-        dense
-        borderless
-        emit-value
-        map-options
-        options-dense
-        options-dark
-        style="max-width: 150px; margin: auto"
-      >
-        <template v-slot:before>
-          <q-icon name="language" />
-        </template>
-      </q-select>
+      <language-switcher></language-switcher>
     </div>
     <div class="col-1 q-mr-sm">Quasar v{{ $q.version }}</div>
   </q-footer>
 </template>
 <script setup>
 import { ref } from 'vue';
-import { useQuasar } from 'quasar';
-import { useI18n } from 'vue-i18n';
+import LanguageSwitcher from 'components/LanguageSwitcher.vue';
 
 const now = ref(new Date().toLocaleString());
-const $q = useQuasar();
-const { locale } = useI18n({ useScope: 'global' });
-const localeOptions = [
-  {
-    value: 'ko-KR',
-    label: '한국어',
-  },
-  {
-    value: 'en-US',
-    label: 'English',
-  },
-];
 </script>
-<style>
-.lang-selector {
-  width: 100px;
-  margin: auto;
-}
-</style>
