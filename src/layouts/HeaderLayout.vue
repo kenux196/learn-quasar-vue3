d74f73895fa86c868ecf12d145e751cc7406e517
kenux196 <kenux.yun@gmail.com>
2024-05-14 22:36:49 +0900

    Modify avatar

--- src/layouts/HeaderLayout.vue
+++ src/layouts/HeaderLayout.vue
@@ -13,11 +13,7 @@
       <q-btn flat>
         <q-icon name="logout"></q-icon>
       </q-btn>
-      <q-avatar>
-        <img
-          src="https://i.namu.wiki/i/htzWGjE4TlsG37rQW2bvfGf3QKEJCVFe5r6sqhQSJPdR_5kn6oiZlgHeJw5TUNIi6RIVhvx0mwriLXAX8UIXjgSey5VjbCgPKVEIjl6qLEDnv4oMSK6M5PEJ5vKYhfCthBr7hoVuwG4nh0w_qbwbsQ.webp"
-        />
-      </q-avatar>
+      <q-avatar color="primary" text-color="white">S</q-avatar>
     </q-toolbar>
   </q-header>
 </template>
