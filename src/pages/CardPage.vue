1727d9ee05f5e71ce93267abbc6af08b40dc712c
kenux196 <kenux.yun@gmail.com>
2024-05-15 21:54:03 +0900

    fix bug & add save

--- src/pages/CardPage.vue
+++ src/pages/CardPage.vue
@@ -4,7 +4,7 @@
     <q-btn dense icon="add" round color="positive" @click="addMember"></q-btn>
   </div>
   <div class="row q-col-gutter-lg">
-    <div v-for="m in members" :key="m.name" class="col-6 col-md-4">
+    <div v-for="m in members" :key="m.mid" class="col-6 col-md-4">
       <q-btn v-if="!m.editable" unelevated dense flat size="sm" color="grey" icon="edit" @click="m.editable = true" />
       <q-btn v-else unelevated dense round size="sm" color="primary" icon="check" @click="store(m.mid)" />
       <member-card
@@ -29,18 +29,10 @@
 import { uid, useQuasar } from 'quasar';
 import { reactive, defineAsyncComponent } from 'vue';
 
-const $q = useQuasar();
-
-// const members = reactive([$q.sessionStorage.getItem('members') || []]);
-const members = reactive([
-  { mid: uid(), name: '디에잇', team: '세븐틴', contact: '010-1234-1234', editable: false },
-  { mid: uid(), name: '도겸', team: '세븐틴', contact: '010-1234-1111', editable: false },
-  { mid: uid(), name: '안유진', team: '아이브', contact: '010-1234-2222', editable: false },
-  { mid: uid(), name: '아이유', team: '아이유', contact: '010-1234-2222', editable: false },
-  { mid: uid(), name: '장원영', team: '아이브', contact: '010-1234-2222', editable: false },
-]);
-
 const MemberCard = defineAsyncComponent(() => import('../components/MemberCard.vue'));
+
+const $q = useQuasar();
+const members = reactive($q.sessionStorage.getItem('members') || []);
 
 const addMember = () => {
   members.unshift({
@@ -48,25 +40,40 @@ const addMember = () => {
     name: '',
     team: '',
     contact: '',
-    editable: '',
+    editable: false,
   });
 };
 
 const update = (mid, key, val) => {
-  console.log(`${mid} : ${key} : ${val}`);
-  const findMember = members.find((m) => m.mid === mid);
+  const findMember = getMember(mid);
   if (findMember) {
     findMember[key] = val;
   }
 };
 
 const store = (mid) => {
-  const findMember = members.find((m) => m.mid === mid);
+  const tempMembers = $q.sessionStorage.getItem('members') || [];
+  const findMember = getMember(mid);
+  const findTempMember = tempMembers.find((m) => m.mid === mid);
+
   if (findMember) {
     findMember.editable = false;
-    $q.sessionStorage.set('members', members);
+
+    if (findTempMember) {
+      findTempMember.name = findMember.name;
+      findTempMember.team = findMember.team;
+      findTempMember.contact = findMember.concat;
+    } else {
+      tempMembers.unshift(findMember);
+    }
+
+    $q.sessionStorage.set('members', tempMembers);
   }
 };
+
+function getMember(mid) {
+  return members.find((m) => m.mid === mid);
+}
 </script>
 
 <style lang="scss" scoped></style>
